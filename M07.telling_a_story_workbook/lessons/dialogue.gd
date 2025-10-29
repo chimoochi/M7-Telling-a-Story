b11e3374bf148e5cb791db7a45bf358655eac24a
Timothy  Nguyen <timothysvn@gmail.com>
2025-10-29 13:07:08 -0700

    M7L6 finish

--- M07.telling_a_story_workbook/lessons/dialogue.gd
+++ M07.telling_a_story_workbook/lessons/dialogue.gd
@@ -1,6 +1,8 @@
 extends Control
 @onready var rich_text_label: RichTextLabel = %RichTextLabel
 @onready var next_button: Button = %NextButton
+@onready var audio_stream_player: AudioStreamPlayer = %AudioStreamPlayer
+
 var dialogue_items : Array[String] = [
 	"starting text",
 	"middle text",
@@ -11,7 +13,18 @@ var currentindex = 0
 
 func show_text():
 	var current_item = dialogue_items[currentindex]
+	
 	rich_text_label.text = current_item
+	rich_text_label.visible_ratio = 0.0
+	
+	var tween := create_tween()
+	var text_appearing_duration := 1.2
+	tween.tween_property(rich_text_label, "visible_ratio", 1.0, text_appearing_duration)
+	
+	var sound_max_offset := audio_stream_player.stream.get_length() - text_appearing_duration
+	var sound_start_position := randf() * sound_max_offset
+	audio_stream_player.play(sound_start_position)
+	tween.finished.connect(audio_stream_player.stop)
 	pass
 func advance():
 	currentindex += 1
